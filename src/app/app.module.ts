4a945d70b54e4574c64b7ecc03169fe7436729a2
YuvalGal2 <muonline.mega@gmail.com>
2018-08-01 19:52:37 +0300

    minor css fixes, and added modal on delete action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
+import { BooksService } from './shared/books.service';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule,ReactiveFormsModule  }   from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { BookComponent } from './books/book/book.component';
+import { EditBookComponent } from './books/edit-book/edit-book.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import { CaptializedTextPipe } from './books/pipes/captialized-text.pipe';
+import { AddBookComponent } from './books/add-book/add-book.component';
+import { DeleteBookComponent } from './books/delete-book/delete-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    BookComponent,
+    EditBookComponent,
+    CaptializedTextPipe,
+    AddBookComponent,
+    DeleteBookComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  entryComponents: [EditBookComponent,AddBookComponent,DeleteBookComponent],
+  providers: [BooksService,CaptializedTextPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
